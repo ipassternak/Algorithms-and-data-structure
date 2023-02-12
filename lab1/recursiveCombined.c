e401f97dcb189259d3669bb21b926934fe751108
ipassternak <retriumpf@gmail.com>
2023-02-12 17:02:03 +0200

    Add `recursiveCombined.c`

---
+++ lab1/recursiveCombined.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+
+double recursive(unsigned int depth, unsigned int lim, double x, double elem) {
+    double res = 0;
+
+    if (depth > lim) {
+        res += x;
+        printf("\n\nSignal function was reached;\n Result: %f\n", res);
+    } else {
+        unsigned int par = 2 * depth;
+        unsigned int numerator = (par - 1) * (par - 1);
+        unsigned int denominator = par * par + par;
+        elem *= x * x * numerator / denominator;
+        depth++;
+
+        printf("\nRecursive descent;\n Depth: %d, Element %d: %f, Result: %f", depth - 1, depth, elem, res);
+        res += elem + recursive(depth, lim, x, elem);
+        printf("\nRecursive return;\n Depth: %d, Element %d: %f, Result: %f", depth - 1, depth, elem, res);
+
+    }
+    return res;
+}
+
+int main() {
+    int depth = 1, lim;
+    double x;
+
+    printf(" Input x from diapason (-1;1): ");
+    scanf("%lf", &x);
+    printf("Set the recursion depth: ");
+    scanf("%u", &lim);
+
+    if (x > -1 && x < 1 && lim > 0 && !(lim % 1)) {
+        double standard = asin(x);
+
+        printf("\nCall a recursion;\n Element %d: %f", depth, x);
+        x = recursive(depth, lim, x, x);
+        printf("\nClosed a recursion;\n");
+
+        double error = (1 - x / standard) * 100;
+        printf("\n Resul: %f\n Standard: %f\n Observational error: %f%%", x, standard, error);
+
+    } else {
+        printf("\nInvalid arguments were passed;\n");
+    }
+    return 0;
+}
